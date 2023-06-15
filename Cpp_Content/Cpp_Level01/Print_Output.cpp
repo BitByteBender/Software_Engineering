5fe3cbdd3ef3a4d0e284156f405c912d054c9614
GodSlayerMode <136563161+GodSlayerMode@users.noreply.github.com>
2023-06-15 13:33:54 -0400

    Update Print_Output.cpp

--- Cpp_Content/Cpp_Level01/Print_Output.cpp
+++ Cpp_Content/Cpp_Level01/Print_Output.cpp
@@ -7,7 +7,7 @@ int main(){
   std::cout<<"I'm Soufiane Sadgali\n";
   std::cout<<"and i'm a software engineer"<<"\n";
   //Output 2:
-  //endl => is the same as \n but it clears the buffer so instead of using multiple endl we use \n to jump to a new line and then we use endl to clear the buffer
+  //endl => is the same as \n but it clears the buffer so instead of using multiple endl we use \n to jump to a new line and then once we are done with our block of code/set_of_instructions use endl to clear the buffer
   std::cout<<"This is"<<" "<<"my first C++"<< " program"<<std::endl;
   return 0;
 }

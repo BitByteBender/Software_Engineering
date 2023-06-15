6a01604734a0eab24fbb62825aca4cdb319fb535
GodSlayerMode <ssadgali2@gmail.com>
2023-06-15 13:22:27 -0400

    Added first course content file Print_Output.cpp

---
+++ Cpp_Content/Cpp_Level01/Print_Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+  //Output 1: This is how you print to the  console
+  //\n => skip this line and jump to a new one
+  std::cout<<"I'm Soufiane Sadgali\n";
+  std::cout<<"and i'm a software engineer"<<"\n";
+  //Output 2:
+  //endl => is the same as \n but it clears the buffer so instead of using multiple endl we use \n to jump to a new line and then we use endl to clear the buffer
+  std::cout<<"This is"<<" "<<"my first C++"<< " program"<<std::endl;
+  return 0;
+}

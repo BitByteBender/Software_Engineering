70056af3683da372446c213732ec7b8e11743dfe
GodSlayerMode <ssadgali2@gmail.com>
2023-06-17 12:57:56 -0400

    Restored All Files in the How-To-Print-To-The-Console Directory

---
+++ Cpp_Content/Cpp_Level01/How_To_Print_To_The_Console/Printing04_Stars.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+  std::cout<<"*   *\n";
+  std::cout<<"*   *\n";
+  std::cout<<"*****";
+  std::cout<<"\n"<<"*   *"<<"\n";
+  std::cout<<"*   *"<<std::endl;
+  return 0;
+}

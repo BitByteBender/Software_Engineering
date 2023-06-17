a13487d15f9982f918f86e381b7a1d49073ac2c3
GodSlayerMode <ssadgali2@gmail.com>
2023-06-17 14:02:19 -0400

    Renamed Directory 03

---
+++ 03_Literals_And_Escape_Sequences/ES_Backslash_DoubleQuotation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	
+	cout<<"My firstname is \"Soufiane\"\n";//Output: My firsname is "Soufiane"
+	cout<<"My lastname is \"Sadgali\"<<endl;//Output: My lastname is "Sadgali"
+	return 0;
+}

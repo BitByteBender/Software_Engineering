c992bb38150a0e686f7255d0f9a62a1d90dcb405
GodSlayerMode <ssadgali2@gmail.com>
2023-06-18 16:02:40 -0400

    Renamed 5 files IntegerType, FloatType, DoubleType, CharacterType, StringType

---
+++ Cpp_Content/Cpp_Level01/04_Variables/04_CharacterType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+
+  char MyFirstChar{'A'};
+  cout<<MyFirstChar<<"\n";
+
+  char MySecondChar;
+  MySecondChar = 'B';
+  cout<<MySecondChar<<"\n";
+  
+  return 0;
+}

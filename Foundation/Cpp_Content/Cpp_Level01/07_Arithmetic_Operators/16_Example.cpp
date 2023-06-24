e47dba4a8333725f9a23d1e509e826c037f2d9df
GodSlayerMode <ssadgali2@gmail.com>
2023-06-24 07:54:46 -0400

    Added 1 file (16_Example to 07_Arithmetic_Operators/

---
+++ Foundation/Cpp_Content/Cpp_Level01/07_Arithmetic_Operators/16_Example.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+//TotalBill and CashPaid
+//remainder to be paid back
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  float TotalBill{0}, CashPaid{0};
+
+  cout<<"What's the TotalBill?: ";
+  cin>>TotalBill;
+
+  cout<<"How much did you pay?: ";
+  cin>>CashPaid;
+
+  float Remainder= TotalBill - CashPaid;
+  cout<<abs(Remainder)<<endl;
+  return 0;
+}
+

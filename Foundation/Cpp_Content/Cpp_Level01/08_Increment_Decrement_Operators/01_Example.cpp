32a04fad9d4fb2900464d86f304960bbd24bc2bd
GodSlayerMode <ssadgali2@gmail.com>
2023-06-27 04:31:18 -0400

    Added 01_Example to 08_Increment_Decrement_Operators Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/08_Increment_Decrement_Operators/01_Example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  short x = 5, y= 15;
+
+  cout<<"x= "<<x<<"\ny= "<<y<<"\n";
+  
+  x++;
+  y--;
+  cout<<"x = "<<x<<"\ny= "<<y<<endl;
+  
+  return 0;
+}

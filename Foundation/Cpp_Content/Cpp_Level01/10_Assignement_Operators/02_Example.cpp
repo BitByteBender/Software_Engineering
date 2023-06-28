c8b8fedeee32e9f7adcdcc50f5dece60d9dc5f37
GodSlayerMode <ssadgali2@gmail.com>
2023-06-28 09:53:21 -0400

    Added 1 file 02_Example.cpp To 10_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/10_Assignement_Operators/02_Example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  short X = 5, Y = 15;
+
+  cout<<X + Y<<"\n";
+  cout<<X - Y<<"\n";
+  cout<<X * Y<<"\n";
+  cout<<X / Y<<"\n";
+  cout<<X % Y<<endl;
+  return 0;
+}

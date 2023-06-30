d15b09549f2b13f51152ac675b7d608ac158ef0c
GodSlayerMode <ssadgali2@gmail.com>
2023-06-30 04:43:39 -0400

    Added 01_ file To 13_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/13_Math_Functions/01_Example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "math.h"
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+  float X{256};
+  cout<<sqrt(X)<<"\n";
+  cout<<sqrt(50)<<"\n";
+  cout<<sqrt(25)<<"\n";
+  cout<<sqrt(90)<<endl;
+  return 0;
+}

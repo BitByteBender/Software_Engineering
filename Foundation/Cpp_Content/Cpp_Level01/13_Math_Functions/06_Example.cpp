e68ce9165e64ae269c6232255ab52ab8e0734401
GodSlayerMode <ssadgali2@gmail.com>
2023-06-30 04:44:40 -0400

    Added 06_ file To 13_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/13_Math_Functions/06_Example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "math.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  float X{-78.52f}, Y{-983.3f}, Z{-56.13f};
+
+  cout<<abs(X)<<"\n"<<abs(Y)<<"\n"<<abs(Z)<<endl;
+  return 0;
+}

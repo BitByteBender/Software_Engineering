cce4caa9e9277a69b84744d4797d8b5aa29f2e6b
GodSlayerMode <ssadgali2@gmail.com>
2023-07-02 12:30:08 -0400

    Added 04_ file to 14_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/14_Structures/04_Example.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <math.h>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+struct stContactDetails{
+  string PhoneNum_1{"+879797971"}, PhoneNum_2{"+1975121887"};
+};
+
+struct stAdr{
+  string Adr_1{"st404, Penthouse"}, Adr_2{"st1001, Bentley Villa"};
+};
+
+struct stPersonInfos{
+  string FirstName{"Milani"};
+  string MiddleName{"Empty"};
+  string LastName{"Olumn"};
+  unsigned short Age{30};
+  string City{"Milan"};
+  string Country{"Italy"};
+  char Gender{'F'};
+  bool isMarried{false};
+  stAdr Adr;
+  stContactDetails ContactDetails;
+};
+
+int main()
+{
+  stPersonInfos Person1, Person2;
+  string PrintStars{"**********************************"};
+
+  cout<<PrintStars<<"\n"
+      <<"First name: "<<Person1.FirstName<<"\n"
+      <<"Last name: "<<Person1.LastName<<"\n"
+      <<"Age: "<<Person1.Age<<"\n"
+      <<"City: "<<Person1.City<<"\n"
+      <<"Country: "<<Person1.Country<<"\n"
+      <<"Gender: "<<Person1.Gender<<"emale"<<"\n"
+      <<"Married: "<<Person1.isMarried<<"\n"
+      <<"Adr: "<<Person1.Adr.Adr_1<<"\n"
+      <<"Phone: "<<Person1.ContactDetails.PhoneNum_1<<"\n"
+      <<PrintStars<<"\n";
+
+  Person2.FirstName = "Souf";
+  Person2.MiddleName = "Slayer";
+  Person2.LastName = "Sadgali";
+  Person2.Age = 29;
+  Person2.City = "Bznk";
+  Person2.Country = "Maruecos";
+  Person2.Gender = 'M';
+  Person2.isMarried = false;
+  Person2.Adr.Adr_1 = "st44, Big Tower";
+  Person2.Adr.Adr_2 = "st47, Penthouse";
+  Person2.ContactDetails.PhoneNum_1 = "+21287968541";
+  Person2.ContactDetails.PhoneNum_2 = "+198524784";
+    
+  cout<<PrintStars<<"\n"
+      <<"First name: "<<Person2.FirstName<<"\n"
+      <<"Last name: "<<Person2.LastName<<"\n"
+      <<"Age: "<<Person2.Age<<"\n"
+      <<"City: "<<Person2.City<<"\n"
+      <<"Country: "<<Person2.Country<<"\n"
+      <<"Gender: "<<Person2.Gender<<"ale"<<"\n"
+      <<"Married: "<<Person2.isMarried<<"\n"
+      <<"Adr1: "<<Person2.Adr.Adr_1<<"\n"
+      <<"Adr2: "<<Person2.Adr.Adr_2<<"\n"
+      <<"Phone1: "<<Person2.ContactDetails.PhoneNum_1<<"\n"
+      <<"Phone2: "<<Person2.ContactDetails.PhoneNum_2<<"\n"
+      <<PrintStars<<"\n";
+  
+    
+  return 0;
+}

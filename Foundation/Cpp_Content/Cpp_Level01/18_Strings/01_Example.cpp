171def854c1685379554f4a051b7ffdc3e8148f7
GodMode <136563161+BitByteBender@users.noreply.github.com>
2023-07-11 13:50:56 -0400

    Update 01_Example.cpp

--- Foundation/Cpp_Content/Cpp_Level01/18_Strings/01_Example.cpp
+++ Foundation/Cpp_Content/Cpp_Level01/18_Strings/01_Example.cpp
@@ -9,16 +9,10 @@ using std::string;
 int main()
 {
   string Fullname{"Empty"};
-  short Age{0};
 
   cout<<"Enter your fullname: ";
   getline(cin, Fullname);
- 
-  cout<<"Enter your age: ";
-  cin>>Age;
 
   cout<<"Fullname: "<<Fullname<<"\n";
-  cout<<"Age: "<<Age<<endl;
-  
   return 0;
 }

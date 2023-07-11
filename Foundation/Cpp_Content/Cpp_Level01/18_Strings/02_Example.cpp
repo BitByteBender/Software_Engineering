dcb7a86df1300b821eb405ebacaae8bab617728a
GodSlayerMode <ssadgali2@gmail.com>
2023-07-11 13:49:12 -0400

    Added 02_Example.cpp To 18_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/18_Strings/02_Example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string st1{"WX"}, st2{"YZ"};
+
+  string st3 = st1 + st2;
+  cout<<st3<<"\n";
+  
+  return 0;
+}

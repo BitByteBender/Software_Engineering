33e354ff738a007ac2a385c391c5a1dd937aefc6
GodSlayerMode <ssadgali2@gmail.com>
2023-07-13 09:31:13 -0400

    Added 04_ Cpp file To 19_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions_Part_1/04_Example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void PrintSquare(){
+  cout<<"**********\n";
+}
+
+int main()
+{
+  PrintSquare();
+  PrintSquare();
+  PrintSquare();
+  PrintSquare();
+  return 0;
+}

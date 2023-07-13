9de338d0bb996ab8f7447f65f708102d9ab6dc1b
GodSlayerMode <ssadgali2@gmail.com>
2023-07-13 09:30:41 -0400

    Added 01_ Cpp file To 19_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions_Part_1/01_Example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+void MyFunc1(){
+  cout<<"This is my first function."<<endl;
+}
+
+void MyFunc2(){
+  cout<<"This is my second function."<<endl;
+}
+
+int main()
+{
+  MyFunc1();
+  MyFunc2();
+  return 0;
+}

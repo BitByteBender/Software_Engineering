13ad175796ad379a8d0a3abf217f51ab07200ce4
GodSlayerMode <ssadgali2@gmail.com>
2023-07-13 09:31:26 -0400

    Added 05_ Cpp file To 19_ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions_Part_1/05_Example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void OutputLetter(){
+  cout<<"I Love Programming!\n"
+      <<"\nI promise to be the best software engineer ever!\n"
+      <<"\nI know it will take some time to practice, but I will achieve my goal.\n\n"
+      <<"Best Regards,\nSoufiane."<<endl;
+}
+
+int main()
+{
+  OutputLetter();
+  return 0;
+}

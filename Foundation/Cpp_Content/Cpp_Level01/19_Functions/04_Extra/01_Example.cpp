31c277381d3c6cd49bf8207d9613ed6edab69279
GodSlayerMode <ssadgali2@gmail.com>
2023-08-15 06:42:14 -0400

    Added 01_Example.cpp To 04_Extra Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra/01_Example.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+struct strHiringInfos
+{
+  short Age{0};
+  bool HasDriverLicense{false};
+  
+};
+
+strHiringInfos MessageReader(){
+  strHiringInfos HiringInfos;
+  cout<<"Enter your age: ";
+  cin>>HiringInfos.Age;
+
+  cout<<"Do you have a driving license(0 / 1): ";
+  cin>>HiringInfos.HasDriverLicense;
+  return HiringInfos;
+}
+
+
+void OutputDecision(strHiringInfos HiringInfos){
+  (HiringInfos.Age >= 19 && HiringInfos.HasDriverLicense) ? cout<<"Hired"<<endl : cout<<"Rejected"<<endl;
+}
+
+int main()
+{
+  OutputDecision(MessageReader());
+  return 0;
+}

256fbc6378ca15588ae37fe84b84601621869fe9
GodSlayerMode <ssadgali2@gmail.com>
2023-08-19 08:46:04 -0400

    Added 13_Example.cpp To 19_Functions/04_Extra/ Directory

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra/13_Example.cpp
@@ -0,0 +1,21 @@
+godmode@GodModeLocker:~/Software_Engineering/Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra$ ./a.out 
+Enter 1st number: 5
+Enter 2nd number: 6
+Enter 3rd number: 7
+Area of a circle described around an arbitrary triangle: 40.0677
+godmode@GodModeLocker:~/Software_Engineering/Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra$ ./a.out 
+Enter a number: 3
+9
+27
+81
+godmode@GodModeLocker:~/Software_Engineering/Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra$ ./a.out 
+Enter a number: 3
+9
+27
+81
+godmode@GodModeLocker:~/Software_Engineering/Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra$ ./a.out 
+Enter a number: 3
+9
+27
+81
+godmode@GodModeLocker:~/Software_Engineering/Foundation/Cpp_Content/Cpp_Level01/19_Functions/04_Extra$ 

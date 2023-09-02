8d2da43087dad6d602ee0a83859e50bc56f0c303
GodSlayerMode <ssadgali2@gmail.com>
2023-09-02 15:30:29 -0400

    Added new file

---
+++ Foundation/Cpp_Content/Cpp_Level01/01_Printing_To_Console/Examples/05_Example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  std::cout<<"******************************"
+	   <<"\nName: Magdy Abraham"
+	   <<"\nAge: 64 Years\n"
+	   <<"Country: Saudi Arabia\n"
+	   <<"City: Jeddah"
+	   <<"\n******************************"
+	   <<std::endl;
+  return 0;
+}

006917d5b7fdf1d208086f14f6907071becb3372
GodSlayerMode <ssadgali2@gmail.com>
2023-09-02 15:30:42 -0400

    Added new file

---
+++ Foundation/Cpp_Content/Cpp_Level01/01_Printing_To_Console/Examples/07_Example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout<<"I Love Programming!\n\n";
+  std::cout<<"I promise to be the best software engineer ever!";
+  std::cout<<"\n\nI know it will take some time to practice and get good at the craft, but I will achieve my goal no matter what.\n\n";
+  std::cout<<"Best Regards,\nFirstname Lastname"<<std::endl;
+  return 0;
+}

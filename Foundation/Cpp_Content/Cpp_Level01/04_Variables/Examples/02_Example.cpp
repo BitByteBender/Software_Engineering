08b8a5ad05574202a45ce73d8ff74797ce8ce74a
GodSlayerMode <ssadgali2@gmail.com>
2023-09-03 13:06:34 -0400

    Added new cpp file

---
+++ Foundation/Cpp_Content/Cpp_Level01/04_Variables/Examples/02_Example.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string Phrase{"My name is "};
+  cout<<Phrase + "Rafael\n";
+  cout<<Phrase + "Soufiane\n";
+  cout<<Phrase + "Hajji"<<endl;
+  return 0;
+}

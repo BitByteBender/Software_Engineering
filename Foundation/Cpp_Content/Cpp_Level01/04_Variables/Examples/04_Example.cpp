aea9d296d794bac6aff7d5d636a9b7ec0e789cd7
GodSlayerMode <ssadgali2@gmail.com>
2023-09-03 13:06:45 -0400

    Added new cpp file

---
+++ Foundation/Cpp_Content/Cpp_Level01/04_Variables/Examples/04_Example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  char Letter, SpecialChar;
+  Letter = 'G';
+  SpecialChar = '\'';
+  cout<<"Letter: "<<Letter<<"\nSpecial Character: "<<SpecialChar<<endl;
+  return 0;
+}

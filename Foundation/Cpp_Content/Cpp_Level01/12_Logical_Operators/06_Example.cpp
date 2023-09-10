c8243c4e28d7d4a4c5593db5b68d59880c3881c0
GodSlayerMode <ssadgali2@gmail.com>
2023-09-09 20:58:45 -0400

    Added new cpp file

---
+++ Foundation/Cpp_Content/Cpp_Level01/12_Logical_Operators/06_Example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  cout<<"12 >= 12 => "<<(12 >= 12)<<"\n";//True = 1
+  cout<<"12 > 7 => "<<(12 > 7)<<"\n";//True = 1
+  cout<<"8 < 6 => "<<(8 < 6)<<"\n";//False = 0
+  cout<<"8 == 8 => "<<(8 == 8)<<"\n";//True = 1
+  cout<<"12 <= 12 => "<<(12 <= 12)<<"\n";//True = 1
+  cout<<"7 == 5 => "<<(7 == 5)<<endl;//False = 0
+  return 0;
+}

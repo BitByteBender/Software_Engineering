17891066ce58f4cd4b9077f6bc1fe67c6a749d1d
GodSlayerMode <ssadgali2@gmail.com>
2023-09-21 18:06:08 -0400

    Added a new file

---
+++ Foundation/Cpp_Content/Cpp_Level01/19_Functions/01_Beginner/09_Example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void MyParagraph(){
+  cout<<"Text here\n\n";
+  cout<<"Another Text here\n\n";
+  cout<<"Insert a text here";
+  cout<<"\n\nInsert another text here"<<endl;
+}
+
+int main()
+{
+  MyParagraph();
+  return 0;
+}

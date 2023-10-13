748a5773286e262e81396aad4ebd6cde819ae3b8
GodSlayerMode <ssadgali2@gmail.com>
2023-10-13 15:42:31 +0100

    Example 08

---
+++ Foundation/Cpp_Content/Cpp_Level01/23_Loops/01-While_Loops/08-Example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  char i;
+
+  i = 65;
+  while (i <= 90) {
+
+    cout<<i<<"\n";
+    i++;
+  }
+  
+  return 0;
+}

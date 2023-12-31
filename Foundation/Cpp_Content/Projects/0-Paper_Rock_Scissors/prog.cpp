4af1b5e6c870b72ddd09e78032ae48ca7a80148c
GodSlayerMode <ssadgali2@gmail.com>
2023-12-31 16:44:11 +0100

    Fixed memory leak on close

--- Foundation/Cpp_Content/Projects/0-Paper_Rock_Scissors/prog.cpp
+++ Foundation/Cpp_Content/Projects/0-Paper_Rock_Scissors/prog.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <string.h>
-#include <string>
+
 /**
+ * Test (1):
+ *  valgrind --leak-check=full ./prog
+ * ==69775== Memcheck, a memory error detector
+ * ==69775== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+ * ==69775== Using Valgrind-3.18.1 and LibVEX; rerun with -h for copyright info
+ * ==69775== Command: ./prog
+ * ==69775== 
+ * [Type run to start or close to exit the program]
+
+ * Command: c
+ * Command: cl
+ * Command: clo
+ * Command: clos
+ * Command: closee
+ * Command: close
+ * Program exited successfully!
+ * ==69775== 
+ * ==69775== HEAP SUMMARY:
+ * ==69775==     in use at exit: 0 bytes in 0 blocks
+ * ==69775==   total heap usage: 4 allocs, 4 frees, 74,760 bytes allocated
+ * ==69775== 
+ * ==69775== All heap blocks were freed -- no leaks are possible
+ * ==69775== 
+ * ==69775== For lists of detected and suppressed errors, rerun with: -s
+ * ==69775== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
  */
 
 using std::cout;
@@ -45,8 +70,13 @@ void onTrigger()
   
   if (strCmp(Cmd, "run")) {
     onStart();
+    free(Cmd);
   } else if (!strCmp(Cmd, "close")) {
+    free(Cmd);
     onTrigger();
+  } else {
+    free(Cmd);
+    onExit();
   }
 
 }
@@ -59,11 +89,13 @@ void onExit()
 
 char *ReadCommand(const char *Msg)
 {
-  char *Cmd = (char *)malloc(sizeof(char) * Max_Cmd_Length);
+  char *Cmd = (char *)malloc(sizeof(char *));
 
-  cout<<Msg;
-  cin.getline(Cmd, Max_Cmd_Length);
-  
+  do {
+    cout<<Msg;
+    cin.getline(Cmd, Max_Cmd_Length);
+  } while (!strCmp(Cmd, "close"));
+
   return (Cmd);
 }
 

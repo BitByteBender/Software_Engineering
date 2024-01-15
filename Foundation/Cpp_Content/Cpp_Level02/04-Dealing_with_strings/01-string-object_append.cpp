678539554d29364db7e3c1f40c4ecfb75b84a7b7
GodSlayerMode <ssadgali2@gmail.com>
2024-01-15 18:05:18 +0100

    string-object: append method

---
+++ Foundation/Cpp_Content/Cpp_Level02/04-Dealing_with_strings/01-string-object_append.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void)
+{
+  string str1 = "My name";
+
+  str1.append(" is ");
+  cout<<str1<<'\n';
+
+  if (str1.at(10) == ' ') {
+    str1.append("soufiane");
+  }
+
+  cout<<str1<<endl;
+
+  uint16_t i = 0;
+
+  for (i = 0; i < str1.length(); i++) {
+    if (i == 11)
+      str1[i] = 'S';
+    
+    if (i == (str1.length() - 1)) {
+      str1.append(" topG");
+      break;
+    }
+  }
+
+  cout<<str1<<endl;
+  return (0);
+}

ea5381e09900e946c7d2ee6e9258cf320876da68
GodSlayerMode <ssadgali2@gmail.com>
2024-01-23 04:56:03 +0100

    algo 42 - tokenize a string and insert them in a vector

---
+++ Foundation/Cpp_Content/Algorithms/02-Level/42-split_string.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+string readText(const char *Msg)
+{
+  string Text{"empty"};
+
+  cout<<Msg;
+  getline(cin, Text);
+
+  return (Text);
+}
+
+vector <string> splitString(string Text, string DELIMITER)
+{
+  vector <string> vWords;
+  
+  short pos = 0;
+  string word;
+  
+  while ((pos = Text.find(DELIMITER)) != std::string::npos) {
+      word = Text.substr(0, pos);
+
+      if (word != "") {
+	vWords.push_back(word);
+      }
+
+      Text.erase(0, pos + DELIMITER.length());
+  }
+
+  word = Text.substr(0, pos);
+  
+  if (word != "") {
+    vWords.push_back(word);
+  }
+  
+  return (vWords);
+}
+
+void printTokens(vector <string> &vWords, short tokens)
+{
+
+  cout<<"tokens = "<<tokens<<'\n';
+  
+  for (string &Word : vWords) {
+    cout<<Word<<'\n';
+  }
+  cout<<endl;
+}
+
+int main(void)
+{
+  string Text = readText("Enter a text: ");
+
+  vector <string> vWords;
+  
+  vWords = splitString(Text, "/");
+  printTokens(vWords, vWords.size());
+
+  return (0);
+}
+

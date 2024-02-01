d72ba25094c81730a5ea74b78b2f221bbfd7900c
GodSlayerMode <ssadgali2@gmail.com>
2024-02-01 15:38:53 +0100

    Init - Bank project phase 1

---
+++ Foundation/Cpp_Content/Projects/1-Bank_prj/0-Bank.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::to_string;
+using std::vector;
+using std::fstream;
+using std::ios;
+using std::ws;
+
+enum enFuncs
+{
+  Display = 0,
+  Insert = 1,
+  Update = 2,
+  Delete = 3,
+  Find = 4,
+  Exit = 5
+};
+
+int main(void)
+{
+  
+  return (0);
+}

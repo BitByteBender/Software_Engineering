9dc84f9040730a5c88c59a16ca1dade1d56e95f4
GodSlayerMode <ssadgali2@gmail.com>
2024-02-09 18:00:37 +0100

    Algo 00 - Number to text (WIP)

--- Foundation/Cpp_Content/Algorithms/03-Level/00-number_to_text.cpp
+++ Foundation/Cpp_Content/Algorithms/03-Level/00-number_to_text.cpp
@@ -3,6 +3,8 @@
 
 using std::cout;
 using std::cin;
+using std::string;
+using std::to_string;
 
 uint16_t readNumber(const char *Msg)
 {
@@ -14,8 +16,23 @@ uint16_t readNumber(const char *Msg)
   return (num);
 }
 
+uint16_t digitCount(uint16_t num)
+{
+  return (to_string(num).length()); 
+}
+
+string convertNumToText(uint16_t num)
+{
+  uint16_t digits = digitCount(num);
+  string Text;
+
+  //Logic WIP
+  return (Text);
+}
+
 int main(void)
 {
-  
+  uint16_t num = readNumber("Enter a number: ");
+  convertNumToText(num);
   return (0);
 }

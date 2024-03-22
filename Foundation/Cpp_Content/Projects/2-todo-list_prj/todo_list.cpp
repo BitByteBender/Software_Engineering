7b6cd670e1d83cb050d4d98d31cd58aff1afada3
GodSlayerMode <ssadgali2@gmail.com>
2024-03-22 03:34:56 +0000

    Testing TextInput

--- Foundation/Cpp_Content/Projects/2-todo-list_prj/todo_list.cpp
+++ Foundation/Cpp_Content/Projects/2-todo-list_prj/todo_list.cpp
@@ -1,107 +1,28 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-using std::vector;
-using std::ios;
 using std::ws;
 
-/*
-enum enRoles
-{
-  Lead = 1,
-  Editor = 2,
-};
 
-struct stUsers
+string prompt(const char* Message)
 {
-  string fullname;
-  enRoles Role;
-};
-*/
+  string Input{"empty"};
 
-struct stContents
-{
-  string Id;
-  string Description;
-};
+  cout<<Message;
+  getline(cin >> ws, Input);
   
-struct stTickets
-{
-  string ticketId;
-  //string ticketEditor;
-  stContents Content;
-  string date;
-};
-
-string promptReader(const char *Msg)
-{
-  string Prompt{"empty"};
-
-  cout<<Msg;
-  getline(cin >> ws, Prompt);
-
-  return (Prompt);
-}
-
-bool isIdUnique(vector <stTickets> &vTickets, string ticketId)
-{
-
-  for (const stTickets &Cnt:vTickets) {
-    if (Cnt.ticketId == ticketId)
-      return (false);
-  }
-  
-  return (true);
-}
-
-
-stTickets readTicket(vector <stTickets> &vTickets)
-{
-  stTickets Ticket;
-
-  Ticket.ticketId = promptReader("Enter a ticket id: ");
-  
-  while (!isIdUnique(vTickets, Ticket.ticketId)) {
-      cout<<"Ticket ID is not unique!\n";
-      Ticket.ticketId = promptReader(">> re-Enter a fresh ticket id: ");
-  }
-  
-  Ticket.Content.Description = promptReader("Enter a content: ");
-  Ticket.date = promptReader("Enter a date: ");
-  
-  return (Ticket);
-}
-
-vector <stTickets> saveTickets(char loader='y')
-{
-  stTickets Ticket;
-  vector <stTickets> vTickets;
-  
-  do {
-    Ticket = readTicket(vTickets);
-    
-    vTickets.push_back(Ticket);
-    
-    loader = char(promptReader("Do you want to keep saving?(y | n): ")[0]);
-    cout<<'\n';
-  } while (loader == 'y' || loader == 'Y');
-
-
-  return (vTickets);
+  return (Input);
 }
 
 int main(void)
 {
-  vector <stTickets> t = saveTickets();
+  string InputTest = prompt("Enter a text: ");
 
-  for (const stTickets &T:t) {
-    cout<<"id: "<<T.ticketId<<endl;
-  }
-  
+  cout<<InputTest<<endl;
   return (0);
 }
+

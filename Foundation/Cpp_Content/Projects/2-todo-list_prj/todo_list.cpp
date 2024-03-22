c00bf982dd4990e3fbcabff9498cd51409de2e73
GodSlayerMode <ssadgali2@gmail.com>
2024-03-22 04:16:08 +0000

    Main menu display

--- Foundation/Cpp_Content/Projects/2-todo-list_prj/todo_list.cpp
+++ Foundation/Cpp_Content/Projects/2-todo-list_prj/todo_list.cpp
@@ -7,6 +7,43 @@ using std::endl;
 using std::string;
 using std::ws;
 
+string extraORNot(string build, bool extra)
+{
+  if (extra) {
+    return (build = '\n' + build + '\n');
+  } else {
+    return (build = build + '\n');
+  }
+}
+
+string tableauBuilder(string style, uint16_t extender, bool extra=false)
+{
+  uint16_t i = 0;
+  string build;
+
+  for (i = 0; i < extender; i++) {
+    build = build + style;
+  }
+  
+  return (extraORNot(build, extra));
+}
+
+void DisplayMainMenu(string style, uint16_t extender)
+{
+  string build = tableauBuilder(style, extender, true);
+
+  cout<<tableauBuilder(style, extender, false)
+      <<"\t\t\t\t    Main Menu"
+      <<build;
+
+  cout<<"\n\t\t1# View"
+      <<" | 2# Add"
+      <<" | 3# Delete"
+      <<" | 4# Update"
+      <<" | 5# Exit"
+      <<build
+      <<endl;
+}
 
 string prompt(const char* Message)
 {
@@ -20,9 +57,6 @@ string prompt(const char* Message)
 
 int main(void)
 {
-  string InputTest = prompt("Enter a text: ");
-
-  cout<<InputTest<<endl;
+  DisplayMainMenu("_", 82);
   return (0);
 }
-

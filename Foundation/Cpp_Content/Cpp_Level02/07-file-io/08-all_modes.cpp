c522c24bc67af241d5d478f9009a80f890f71a76
GodSlayerMode <ssadgali2@gmail.com>
2024-04-22 14:34:48 +0100

    Test color palette

--- Foundation/Cpp_Content/Cpp_Level02/07-file-io/08-all_modes.cpp
+++ Foundation/Cpp_Content/Cpp_Level02/07-file-io/08-all_modes.cpp
@@ -32,21 +32,71 @@ string dataReader(const char *Message)
   return (Input);
 }
 
+void ColorsDisplayer()
+{
+  cout<<"[Color's Menu]:"
+      <<"\n[[0]> Red, [1]> Black, [2]> White]\n[[3]> Gray, [4]> Blue, [5]> Yellow]\n"
+      <<endl;
+}
+
+stPersonData PersonRecord()
+{
+  stPersonData Person;
+  uint16_t ColorPicker = 0;
+  
+  Person.Fullname = dataReader("What's your name?: ");
+  Person.Age = stoi(dataReader("How old are you?: "));
+
+  ColorsDisplayer();
+  ColorPicker = stoi(dataReader("What's your favourite color?: "));
+  Person.FavColor = (enColors) ColorPicker;
+  
+  return (Person);
+}
+
+string ColorPicker(enColors Color)
+{
+  string ColorPicker;
+  
+  switch (Color) {
+  case (enColors::Red):
+    ColorPicker = "Red";
+    break;
+  case (enColors::Black):
+    ColorPicker = "Black";
+    break;
+  case (enColors::White):
+    ColorPicker = "White";
+    break;
+  case (enColors::Gray):
+    ColorPicker = "Gray";
+    break;
+  case (enColors::Blue):
+    ColorPicker = "Blue";
+    break;
+  case (enColors::Yellow):
+    ColorPicker = "Yellow";
+    break;
+  default:
+    ColorPicker = "None";
+  }
+
+  return (ColorPicker);
+}
 
 int main(void)
 {
-  string testArr[5];
-  uint16_t i = 0, counter = 0;
-
-  for (i = 0; i < 5; ++i) {
-    cout<<'['<<i<<']';
-    testArr[i] = dataReader("enter a test: ");
+  stPersonData Persons[3];
+  uint16_t i = 0;
+  
+  for (i = 0; i < 3; ++i) {
+    Persons[i] = PersonRecord();
   }
-
-  while (i != 0) {
-    cout<<'['<<counter<<"]: "<<testArr[counter]<<endl;
-    counter++;
-    i--;
+  
+  for (const stPersonData &P : Persons) {
+    cout<<"Fullname: "<<P.Fullname<<'\n'
+	<<"Age: "<<P.Age<<'\n'
+	<<"Favourite Color: "<<ColorPicker(P.FavColor)<<'\n'<<endl;
   }
   
   return (0);

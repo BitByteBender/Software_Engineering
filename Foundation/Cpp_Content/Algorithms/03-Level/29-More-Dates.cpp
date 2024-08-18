183eef10230de3ba0e490b0a661e7ebb899ad015
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-18 16:28:53 +0100

    Algo 29 - More Dates Info(WIP)

--- Foundation/Cpp_Content/Algorithms/03-Level/29-More-Dates.cpp
+++ Foundation/Cpp_Content/Algorithms/03-Level/29-More-Dates.cpp
@@ -18,11 +18,26 @@ uint16_t DataPrompt(const char *Message)
   cout<<Message;
   cin>>Num;
   
-  return (0);
+  return (Num);
+}
+
+stDate FeedData()
+{
+  stDate Date;
+
+  Date.Year = DataPrompt("Enter Year: ");
+  Date.Month = DataPrompt("Enter Month: ");
+  Date.Day = DataPrompt("Enter Day: ");
+  
+  return (Date);
 }
 
 int main(void)
 {
+  stDate Date;
+  Date = FeedData();
+
+  cout<<"\nDate(Entry): "<<Date.Day<<'/'<<Date.Month<<'/'<<Date.Year<<'\n'<<endl;
   
   return (0);
 }

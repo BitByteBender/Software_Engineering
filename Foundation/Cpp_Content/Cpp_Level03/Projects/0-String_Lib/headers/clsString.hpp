f3992393d3d325a738bf13333a692f9fbaae09d7
Soufiane Sadgali <ssadgali2@gmail.com>
2025-03-06 13:05:34 +0000

    Adding FindPos, Substr methods and Updating the GetWords method

--- Foundation/Cpp_Content/Cpp_Level03/Projects/0-String_Lib/headers/clsString.hpp
+++ Foundation/Cpp_Content/Cpp_Level03/Projects/0-String_Lib/headers/clsString.hpp
@@ -3,16 +3,21 @@
 
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <cstdlib>
+#include <string.h>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 class clsString
 {
 private:
   const char *m_Value = nullptr;
-
+  static const char *m_Global_Value;
+  
 public:
   clsString() : m_Value(nullptr) {}
   clsString(const char *Val) : m_Value(Val) {}
@@ -20,8 +25,16 @@ public:
   void setValue(const char *Val);
   const char *getValue() const;
 
+  static void setGValue(const char *Val);
+  static const char *getGValue();
   static uint16_t CountWords(const char *Val);
   uint16_t CountWords() const;
-};
 
+  static uint16_t FindPos(const char *DELIM);
+  static const char *Substr(uint16_t From, uint16_t To);
+  static const char *Erase(uint16_t From, uint16_t To);
+  
+  static vector <string> GetWords(const char *Val, const char *DELIM);
+  vector <string> GetWords(const char *DELIM) const;
+};
 #endif /* CLSSTRING_H */

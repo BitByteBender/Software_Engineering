f686a812a49ad7bcf912ee452601c4c4fcf30986
Soufiane Sadgali <ssadgali2@gmail.com>
2025-04-01 07:39:45 +0000

    Stack DS Creation

--- Foundation/Cpp_Content/Projects/03-DSA-Lib/main.cpp
+++ Foundation/Cpp_Content/Projects/03-DSA-Lib/main.cpp
@@ -1,59 +1,59 @@
-#include "Header/DS/clsMyQueue.hpp"
+#include "Header/DS/clsMyStack.hpp"
 
 int main(void)
 {
-  clsMyQueue<int> MyQueue;
-
-  MyQueue.push(10);
-  MyQueue.push(50);
-  MyQueue.push(100);
-  MyQueue.push(150);
-  MyQueue.push(200);
-  MyQueue.push(250);
-
-  cout<<"Queue: ";
-  MyQueue.Print();
+  clsMyStack<int> MyStack;
   
-  cout<<"Queue Size: "<<MyQueue.Size()<<'\n';
-  cout<<"Queue Front: "<<MyQueue.front()<<'\n';
-  cout<<"Queue Back: "<<MyQueue.back()<<'\n';
+  MyStack.push(10);
+  MyStack.push(50);
+  MyStack.push(100);
+  MyStack.push(150);
+  MyStack.push(200);
+  MyStack.push(250);
 
-  MyQueue.pop();
-  cout<<"\n>> An Item from the queue just popped!\n"<<endl;
+  cout<<"Stack: ";
+  MyStack.Print();
   
-  cout<<"Queue After<POP> (FIFO Operation):\n";
-  MyQueue.Print();
+  cout<<"Stack Size: "<<MyStack.Size()<<'\n';
+  cout<<"Stack Top: "<<MyStack.Top()<<'\n';
+  cout<<"Stack Bottom: "<<MyStack.Bottom()<<'\n';
+
+  MyStack.pop();
+  cout<<"\n>> An Item from the stack just popped!\n"<<endl;
   
-  cout<<"Queue Size: "<<MyQueue.Size()<<'\n';
-  cout<<"Queue Front: "<<MyQueue.front()<<'\n';
-  cout<<"Queue Back: "<<MyQueue.back()<<'\n';
+  cout<<"Stack After<POP> (LIFO Operation):\n";
+  MyStack.Print();
+  
+  cout<<"Stack Size: "<<MyStack.Size()<<'\n';
+  cout<<"Stack Top: "<<MyStack.Top()<<'\n';
+  cout<<"Stack Bottom: "<<MyStack.Bottom()<<'\n';
 
   cout<<"\n[Extension]>:"<<endl;
-  MyQueue.Reverse();
-  cout<<"Queue After Reverse: ";
-  MyQueue.Print();
+  MyStack.Reverse();
+  cout<<"Stack After Reverse: ";
+  MyStack.Print();
 
-  cout<<"Item(2): "<<MyQueue.GetItem(2)<<'\n';
+  cout<<"Item(2): "<<MyStack.GetItem(2)<<'\n';
   
-  MyQueue.UpdateItem(2, 500);
-  cout<<"Queue after updating Item(2) to 500: \n";
-  MyQueue.Print();
+  MyStack.UpdateItem(2, 500);
+  cout<<"Stack after updating Item(2) to 500: \n";
+  MyStack.Print();
   
-  MyQueue.InsertAfter(2, 900);
-  cout<<"\nQueue After inserting 900 after Item(2): \n";
-  MyQueue.Print();
+  MyStack.InsertAfter(2, 900);
+  cout<<"\nStack After inserting 900 after Item(2): \n";
+  MyStack.Print();
 
-  MyQueue.InsertAtFront(2500);
-  cout<<"\nQueue after inserting 2500 at front: \n";
-  MyQueue.Print();
+  MyStack.InsertAtTop(2500);
+  cout<<"\nStack after inserting 2500 at top: \n";
+  MyStack.Print();
 
-  MyQueue.InsertAtBack(3000);
-  cout<<"\nQueue after inserting 3000 at back: \n";
-  MyQueue.Print();
+  MyStack.InsertAtBottom(3000);
+  cout<<"\nStack after inserting 3000 at bottom: \n";
+  MyStack.Print();
 
-  MyQueue.Clear();
-  cout<<"\nQueue after clearing: \n";
-  MyQueue.Print();
+  MyStack.Clear();
+  cout<<"\nStack after clearing: \n";
+  MyStack.Print();
     
   return (0);
 }

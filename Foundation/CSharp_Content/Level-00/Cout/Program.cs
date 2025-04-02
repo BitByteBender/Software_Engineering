044e57da409d4cdc26eb8a3722fa3a6c4b10d28f
Soufiane Sadgali <ssadgali2@gmail.com>
2025-04-02 23:48:12 +0000

    Console Output in CSharp

---
+++ Foundation/CSharp_Content/Level-00/Cout/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+	static void Main()
+	{
+	    // this works like std::cout<<"Anything here"<<endl;
+	    Console.WriteLine("This is a test of .NET Framework on Linux!\n");
+	    Console.WriteLine("Tried to use Wine but it didn\'t work :(");
+	    Console.WriteLine("But i got it working using mono :)");
+	    Console.WriteLine("Lets try to calculate this: " + (15 + 5));
+
+	    // this works like system pause in C++
+	    Console.ReadKey();
+	}
+    }
+}
